54ed9e5a7ffb0be4b604b8d89744dd4b55094c06
Shobi <s.oadisa.dev@gmail.com>
2022-04-07 07:23:42 -0700

    Task 4 Retry

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  *main - Entry point
  *
  *Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	char str_1[] = "\"Programming is like building a multilingual puzzle";
-	
 	puts(str_1);
-
 	return (0);
 }
