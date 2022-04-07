da3504cc3394c84cb2b4ef9930ce1cebdfe99589
Shobi <s.oadisa.dev@gmail.com>
2022-04-07 08:06:48 -0700

    Task 6 Retry

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	printf("Size of char: %2d \n", sizeof(char));
-	printf("Size of int: %2d \n", sizeof(int));
-	printf("Size of long int: %2d \n", sizeof(long int));
-	printf("Size of long long int: %2d \n", sizeof(long long int));
-	printf("Size of float: %2d \n", sizeof(float));
+	printf("Size of char: %2d byte(s)\n", sizeof(char));
+	printf("Size of int: %2d byte(s)\n", sizeof(int));
+	printf("Size of long int: %2d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %2d byte(s)\n", sizeof(long long int));
+	printf("Size of float: %2d byte(s)\n", sizeof(float));
 
 	return (0);
 }
