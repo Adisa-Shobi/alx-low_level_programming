00c6651d6dfa0528d65453fc76c0bf260edad33d
Shobi <s.oadisa.dev@gmail.com>
2022-04-07 10:24:24 -0700

    New shell prints to stdout and returns 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#inculde <stdio.h>
+/**
+ *main - Entry function
+ *
+ *Output: 1(Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+	return (1);
+}
