576b1a93b622e34c79d25edf7ce595eb2f932f64
Shobi <s.oadisa.dev@gmail.com>
2022-04-07 08:02:08 -0700

    New shell prints size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+/**
+ *main -Entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("Size of char: %2d \n", sizeof(char));
+	printf("Size of int: %2d \n", sizeof(int));
+	printf("Size of long int: %2d \n", sizeof(long int));
+	printf("Size of long long int: %2d \n", sizeof(long long int));
+	printf("Size of float: %2d \n", sizeof(float));
+
+	return (0);
+}
