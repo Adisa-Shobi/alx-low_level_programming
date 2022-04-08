d588f5fdfe7d36a552ac3ca62ee0b20866d40192
Shobi <s.oadisa.dev@gmail.com>
2022-04-08 11:20:50 -0700

    Task 8 Retry

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int  main(void)
 	const int SIZE = 16;
 
 	char letters[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-			  'A', 'B', 'C', 'D', 'E', 'F'};
+			  'a', 'b', 'c', 'd', 'e', 'f'};
 
 	int index;
 
