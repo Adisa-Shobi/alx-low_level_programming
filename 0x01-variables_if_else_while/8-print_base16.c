ac15a846ed4e5118f72c73e6b8e0fe882bff9193
Shobi <s.oadisa.dev@gmail.com>
2022-04-08 11:18:00 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: 0(Success)
+*/
+int  main(void)
+{
+	const int SIZE = 16;
+
+	char letters[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+			  'A', 'B', 'C', 'D', 'E', 'F'};
+
+	int index;
+
+	for (index = 0 ; index < SIZE ; index++)
+	{
+		putchar(letters[index]);
+	}
+	putchar('\n');
+	return (0);
+}
