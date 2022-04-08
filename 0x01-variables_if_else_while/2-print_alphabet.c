b23c5acd05dba3d2726d68d3f4ac2afa580d9de8
Shobi <s.oadisa.dev@gmail.com>
2022-04-08 08:44:08 -0700

    Task 2 Retry

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,14 +9,16 @@ int  main(void)
 {
 	const int SIZE = 26;
 
-	char letters[SIZE] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+	char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 			      'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
 			      'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
 			      'y', 'z'};
 
+	int index;
+
 	for (index = 0 ; index <= SIZE ; index++)
 	{
-		putchar(letters[index])
+		putchar(letters[index]);
 	}
 	return (0);
 }
