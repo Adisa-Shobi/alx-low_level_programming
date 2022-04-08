19341ab3130d75d0867b956878e44f78d6804502
Shobi <s.oadisa.dev@gmail.com>
2022-04-08 10:02:07 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: 0(Success)
+*/
+int  main(void)
+{
+	const int SIZE = 26;
+
+	char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+			      'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+			      'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+			      'y', 'z'};
+
+	int index;
+
+	for (index = SIZE - 1 ; index <= 0 ; index--)
+	{
+		putchar(letters[index]);
+	}
+	putchar('\n');
+	return (0);
+}
