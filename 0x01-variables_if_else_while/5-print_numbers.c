27d8c1df4e9bcc9978b2c010896bcd5c92a7cccf
Shobi <s.oadisa.dev@gmail.com>
2022-04-08 09:47:25 -0700

    Task 5 Retry

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,9 +11,11 @@ int main(void)
 
 	int index;
 
+	int numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
 	for (index = 0; index < SIZE ; index++)
 	{
-		putchar(index);
+		putchar(number[index]);
 	}
 	putchar('\n');
 	return (0);
