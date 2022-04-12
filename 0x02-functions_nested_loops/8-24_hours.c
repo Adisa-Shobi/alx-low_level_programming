a8e3bbb6b80043da9fa38b7b159ad1181220e420
Shobi <s.oadisa.dev@gmail.com>
2022-04-12 11:04:08 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints every minute of the day
+ *
+ *Return: void
+ */
+void jack_bauer(void)
+{
+	int hours;
+
+	int mins;
+
+	for (hours = 0 ; hours < 24 ; hours++)
+	{
+		for (mins = 0 ; mins < 59 ; mins++)
+		{
+			if (hours < 10)
+				putchar('0');
+			puchar(hours);
+			if (mins < 10)
+				putchar('0');
+			putchar(mins);
+		}
+	}
+}
