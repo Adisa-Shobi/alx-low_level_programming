99f5beb39f5878792b7a3641d54657a9a6dde803
Shobi <s.oadisa.dev@gmail.com>
2022-04-12 06:51:49 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints lowercase alphabet followed by a new line
+ *
+ *Return: 0(success)
+ */
+void print_alphabet(void)
+{
+	for (c = 'a'; c <= 'z' ; ++c )
+	{
+		_putchar(c);
+	}
+	return (0);
+}
