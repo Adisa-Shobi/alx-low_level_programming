f60b3a1ed7bf92b712c4c13cf62eefe747f92cc0
Shobi <s.oadisa.dev@gmail.com>
2022-04-12 17:22:58 -0700

    task 8 Retry

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,7 +20,7 @@ void jack_bauer(void)
 	for (hours_1 = 0 ; hours_1 <= 2 ; hours_1++)
 	{
 		if (hours_1 == 2)
-			max = 4;
+			max = 3;
 		else
 			max = 9;
 		for (hours_2 = 0 ; hours_2 <= max ; hours_2++)
@@ -31,7 +31,7 @@ void jack_bauer(void)
 				{
 					_putchar(hours_1 + '0');
 					_putchar(hours_2 + '0');
-					_putchar(3);
+					_putchar(':');
 					_putchar(mins_1 + '0');
 					_putchar(mins_2 + '0');
 					_putchar('\n');
