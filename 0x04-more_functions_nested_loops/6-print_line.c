eb3a9685aa64695510ba5987d3dfdeac9c112ecb
Shobi <s.oadisa.dev@gmail.com>
2022-04-14 05:53:30 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_line - prints the char - n times
+ *
+ *@n: The number of - chars
+ *Return: void
+ */
+void print_line(int n)
+{
+	int index = 0;
+
+	if (n < 0)
+	{
+		while ( index < n)
+		{
+			_putchar('-');
+			index++;
+		}
+	}
+	_putchar('\n');
+}
