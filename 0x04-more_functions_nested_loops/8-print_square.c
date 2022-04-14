072082c3e7290c7f855a0d15771ae6d90686b0ef
Shobi <s.oadisa.dev@gmail.com>
2022-04-14 06:50:52 -0700

    TAsk 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *print_diagonal - prints square using # char
+ *
+ *@n: The length of the diagonal
+ *Return: void
+ */
+void print_square(int n)
+{
+	int index;
+
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			index = 0;
+
+			while (index < n)
+			{
+				_putchar('#');
+				index++;
+			}
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
