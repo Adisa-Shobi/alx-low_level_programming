99edd9e4d6e52b52f52e015fdfaecf9cf2f81e8c
Shobi <s.oadisa.dev@gmail.com>
2022-04-20 11:52:33 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_array - Prints the first n integers in an array
+ *@a: array of integers
+ *@n: Number of integers to print
+ *Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", *(a + i));
+		if (i < (n - 1))
+			printf(", ");
+	}
+}
