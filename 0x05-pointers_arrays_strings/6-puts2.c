75b388806e1267e465fe224d7c7f0c0b3444ad66
Shobi <s.oadisa.dev@gmail.com>
2022-04-20 10:54:58 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "_strlen.c"
+
+/**
+ *puts2 - Prints evry other char of a str
+ *
+ *@str: The string input
+ *Return: void
+ */
+void puts2(char *str)
+{
+	int i, len;
+
+	len = _strlen(str) - 1;
+	for (i = 0 ; i <= len ; i+=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
