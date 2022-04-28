cff73572b02548fa5f695b05c33067584d83dcdc
Shobi <s.oadisa.dev@gmail.com>
2022-04-28 06:30:22 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - finds x raised to power y
+ *@x: int
+ *@y: exponent
+ *Return: int x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y >= 0)
+	{
+		if (y == 0)
+			return (1);
+		return(x * _pow_recursion(x, y - 1));
+	}
+	else
+		return (-1);
+}
