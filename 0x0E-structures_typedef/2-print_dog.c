42dbf97e154316d3035fc2a4093b28c43199f6c9
Shobi <s.oadisa.dev@gmail.com>
2022-05-11 08:55:21 -0700

    Task 2 Retry

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,17 +11,9 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name != NULL)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: (nil)");
-		if (d->age)
-			printf("Age: %f\n", d->age);
-		else
-			printf("Age: (nil)");
-		if (d->owner)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: (nil)");
+		printf("Name: %s\nAge: %f\nOwner: %s\n",
+		       (*d).name != NULL ? d->name : "(nil)",
+		       (*d).age,
+		       (*d).owner != NULL ? d->owner : "(nil)");
 	}
 }
--- 0x0E-structures_typedef/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void print_dog(struct dog *d);
-
-#endif /* MAIN_H */
