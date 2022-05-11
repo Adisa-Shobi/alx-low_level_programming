f2222daaf9f94d9b758633a9bdbc245a9b40f77a
Shobi <s.oadisa.dev@gmail.com>
2022-05-11 11:18:06 -0700

    Task 4 Retry

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,14 +18,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
-	(*my_dog).name = malloc(sizeof(name));
+	my_dog->name = malloc((sizeof(char) * strlen(name)) + 1);
 	if (my_dog->name == NULL)
 	{
 		free(my_dog);
 		return (NULL);
 	}
 	my_dog->name = strcpy(my_dog->name, name);
-	(*my_dog).owner = malloc(sizeof(owner));
+	my_dog->owner = malloc((sizeof(char) * strlen(owner)) + 1);
 	if (my_dog->owner == NULL)
 	{
 		free(my_dog->name);
