507f49f162f7fe1a9995d8f71976372eff19b23e
Shobi <s.oadisa.dev@gmail.com>
2022-05-11 09:13:05 -0700

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,13 @@
  *@owner: The name of dog's owner
  *Description: Each instance describes a dog.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char  *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
