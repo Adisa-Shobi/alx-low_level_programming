04474ca71eb90e3200e121a8fb6b6421ca0cbce1
Shobi <s.oadisa.dev@gmail.com>
2022-05-13 10:56:00 -0700

    Task 2 Retry

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,9 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (cmp == NULL)
-		return (-1);
-	if (size <= 0)
+	if (size <= 0 || cmp == NULL || array == NULL)
 		return (-1);
 	while (i < size)
 	{
