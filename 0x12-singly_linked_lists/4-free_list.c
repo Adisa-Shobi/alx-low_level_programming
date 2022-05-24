5a37858e69535ceb351b80b79466194fc8d3c181
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-05-24 16:19:05 +0000

    Writing a function that prints before main is executed

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *free_list - frees the memory allocated to a singly linked list
+ *@head: The head pointer of the list
+ *Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
