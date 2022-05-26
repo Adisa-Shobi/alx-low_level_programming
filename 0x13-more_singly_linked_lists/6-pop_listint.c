668bfea2c7b84012508ec98f1d599b83fa90c28d
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-05-26 15:56:02 +0000

    Correction

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,7 +13,11 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int data;
 
-	temp = *head;
+	if (*head == NULL)
+	{
+		return (0);
+	}
+		temp = *head;
 	*head = (*head)->next;
 	data = temp->n;
 	free(temp);
