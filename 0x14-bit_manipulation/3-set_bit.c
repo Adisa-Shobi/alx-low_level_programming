e56ee4ce2a0453e39535e07574844d71ba46be6e
Shobi <s.oadisa.dev@gmail.com>
2022-05-31 14:06:32 -0700

    Task 3 correction

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,14 +2,14 @@
 #include <stddef.h>
 
 /**
- *set-bit - sets the bit at a given index to i
+ *set_bit - sets the bit at a given index to i
  *@n: pointer to unsigned long int
  *@index: the index to be set to 1
  *Return: 1 if successful, -1 if not
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL)
+	if (n == NULL || index > 31)
 		return (-1);
 	*n |= (1 << index);
 	return (1);
