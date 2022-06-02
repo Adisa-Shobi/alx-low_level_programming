3fd07fc734dfaefbf3a48561ab1f72430a84bc35
Shobi <s.oadisa.dev@gmail.com>
2022-06-02 10:53:00 -0700

    Advanced task correction

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -15,46 +15,26 @@ int main(int argc, char **argv)
 {
 	Elf64_Ehdr *header;
 	char *filename;
-	int fd_header, rstatus, is_elf;
+	int fd_header;
 
 	if (argc != 2)
 	{
-		dprintf(STDERR_FILENO, "Usage: elf_header filename\n");
+		dprintf(STDERR_FILENO, "Usage: elf_header %s\n", argv[1]);
 		exit(98);
 	}
 	filename = argv[1];
-	fd_header = open(filename, O_RDONLY);
-	if (fd_header < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Failed to open file %s\n",
-			filename);
-		exit(98);
-	}
+	fd_header = open_elf(filename);
 	header = malloc(sizeof(*header));
 	if (header == NULL)
 	{
-		close(fd_header);
+		close_elf(fd_header);
 		free(header);
 		dprintf(STDERR_FILENO,
 			"Error: Failed to allocate header memory\n");
 		exit(98);
 	}
-	rstatus = read(fd_header, header, sizeof(*header));
-	if (rstatus < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Failed to read fd %d\n",
-			fd_header);
-		exit(98);
-	}
-	is_elf = check_elf(header->e_ident);
-	if (is_elf == 0)
-	{
-		free(header);
-		close(fd_header);
-		dprintf(STDERR_FILENO, "Error: %s is not an ELF file\n",
-			filename);
-		exit(98);
-	}
+	read_elf(fd_header, header);
+	check_elf(header, fd_header, filename);
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
@@ -62,28 +42,85 @@ int main(int argc, char **argv)
 	print_version(header->e_ident);
 	print_osabi(header->e_ident);
 	print_abiversion(header->e_ident);
-	print_type(header->e_type);
+	print_type(header->e_type, header->e_ident);
 	print_address(header->e_entry, header->e_ident);
+	close_elf(fd_header);
+	free(header);
 	return (0);
 }
 
 /**
- *check_elf - Checks to if header is elf
+ *open_elf - reads the elf file
  *
- *@e_ident: A pointer to array of ELF magic numbers
+ *@filename: The file name
+ *Return: File descpiptor
+ */
+int open_elf(char *filename)
+{
+	int fd_header = open(filename, O_RDONLY);
+
+	if (fd_header < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Failed to open file %s\n",
+			filename);
+		exit(98);
+	}
+	return (fd_header);
+}
+
+/**
+ *close_elf - closes the ELF file descriptor
+ *@fd_header: File descriptor
+ *Return: void
+ */
+void close_elf(int fd_header)
+{
+	if (close(fd_header) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Failed to close fd %d",
+			fd_header);
+		exit(98);
+	}
+}
+
+/**
+ *read_elf - Read the file fd points to into buffer
+ *@fd_header: File descriptor to header
+ *@header: Head of ELF file
+ *Return: header
+ */
+void read_elf(int fd_header, Elf64_Ehdr *header)
+{
+	int rstatus = read(fd_header, header, sizeof(*header));
+
+	if (rstatus < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Failed to read fd %d\n",
+			fd_header);
+		exit(98);
+	}
+}
+
+/**
+ *check_elf - Checks to if header is elf
+ *@fd_header: ELF header file description
+ *@filename: Name of ELF
+ *@header: A pointer to array of ELF magic numbers
  *Return: 1 if ELF, 0 if not
  */
-int check_elf(unsigned char *e_ident)
+void check_elf(Elf64_Ehdr *header, int fd_header, char *filename)
 {
-	if ((e_ident[EI_MAG0] == 127) &&
-	    (e_ident[EI_MAG1] == 'E') &&
-	    (e_ident[EI_MAG2] == 'L') &&
-	    (e_ident[EI_MAG3] == 'F'))
+	if ((header->e_ident[EI_MAG0] != 127) ||
+	    (header->e_ident[EI_MAG1] != 'E') ||
+	    (header->e_ident[EI_MAG2] != 'L') ||
+	    (header->e_ident[EI_MAG3] != 'F'))
 	{
-		return (1);
+		free(header);
+		close_elf(fd_header);
+		dprintf(STDERR_FILENO, "Error: %s is not an ELF file\n",
+			filename);
+		exit(98);
 	}
-	else
-		return (0);
 }
 
 /**
@@ -120,7 +157,7 @@ int print_magic(unsigned char *e_ident)
 int print_class(unsigned char *e_ident)
 {
 	printf("  Class:                             ");
-	switch(e_ident[EI_CLASS])
+	switch (e_ident[EI_CLASS])
 	{
 	case ELFCLASSNONE:
 		printf("Invalid\n");
@@ -146,7 +183,7 @@ int print_class(unsigned char *e_ident)
 void print_data(unsigned char *e_ident)
 {
 	printf("  Data:                              ");
-	switch(e_ident[EI_DATA])
+	switch (e_ident[EI_DATA])
 	{
 	case ELFDATANONE:
 		printf("Unknown data format\n");
@@ -163,7 +200,7 @@ void print_data(unsigned char *e_ident)
 }
 
 /**
- *print_version print sthe version of the elf file
+ *print_version - print sthe version of the elf file
  *
  *@e_ident: An array of magic numbers
  *Return: void
@@ -235,15 +272,18 @@ void print_abiversion(unsigned char *e_ident)
 
 /**
  *print_type - Prints the type of ELF
- *
+ *@e_ident: An array of magic numbers
  *@e_type: type
  *Return: void
  */
-void print_type(unsigned int e_type)
+void print_type(unsigned int e_type, unsigned char *e_ident)
 {
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+		e_type >>= 8;
+
 	printf("  Type:                              ");
 
-	switch(e_type)
+	switch (e_type)
 	{
 	case ET_NONE:
 		printf("NONE (None)\n");
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,20 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <sys/types.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void transfer_bytes(int, int, char *, char *);
 
-int check_elf(unsigned char *e_ident);
+void check_elf(Elf64_Ehdr *header, int fd_header, char *filename);
+int open_elf(char *filename);
+void read_elf(int fd_header, Elf64_Ehdr *header);
 int print_magic(unsigned char *e_ident);
 int print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
 void print_abiversion(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type);
+void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_address(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int fd_header);
 
 #endif /* MAIN_H */
