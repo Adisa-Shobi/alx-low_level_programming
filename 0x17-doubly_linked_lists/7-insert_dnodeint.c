acb41c7736b9a303541c7435ed351648752b871f
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-07-07 15:41:12 +0000

    Task 7: Inserts a new node an index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ *insert_dnodeint_at_index - Inserts a node at specified index
+ *@n: The data stored in node
+ *@idx: Index to store the new node
+ *@h: Head of the list
+ *Return: A pointer to the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *ptr = *h;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	while (ptr)
+	{
+		if (i == idx)
+		{
+			new->prev = ptr->prev;
+			new->next = ptr;
+			ptr->prev->next = new;
+			ptr->prev = new;
+			return (new);
+		}
+		ptr = ptr->next;
+		i++;
+	}
+	return (NULL);
+}
