a624d5502d6fa065054c4fb29b78d1cf937aeceb
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-07-29 13:55:02 +0000

    Task 4 Retry

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,6 +5,7 @@
  *
  *@ht: The hash table being queried
  *@key: The key being searched for
+ *Return: The value being searched for, or NULL if not found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
@@ -15,13 +16,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	node = (ht->array)[node_index];
 	if (!node)
 		return (NULL);
-	while (node->next && (strcmp(node->key, key) != 0))
+	while (node)
 	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
 		node = node->next;
-	}
-	if (strcmp(node->key, key) == 0)
-	{
-		return (node->value);
 	}
 	return (NULL);
 }
